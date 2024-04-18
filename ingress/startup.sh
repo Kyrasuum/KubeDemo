94b91e5c33fa1185b84d4d4e641332c2a5f49949
phil <plorentz7@gmail.com>
2024-04-18 11:32:37 -0400

    adding more configuration to nginx ingress

--- ingress/startup.sh
+++ ingress/startup.sh
@@ -1,15 +1,5 @@
 #!/bin/sh
-if [ "$NAMESERVER" == "" ]; then
-    export DNS_SRV=$(cat /etc/resolv.conf |grep -i '^nameserver'|head -n1|cut -d ' ' -f2)
-fi
-
-echo "Nameserver is: $DNS_SRV"
-
-echo "Copying nginx config"
-envsubst '$DNS_SRV' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
-
-echo "Using nginx config:"
-cat /etc/nginx/nginx.conf
+echo resolver $(awk 'BEGIN{ORS=" "} $1=="nameserver" {print $2";"}' /etc/resolv.conf) > /etc/nginx/resolvers.conf
 
 echo "Starting nginx"
 exec nginx -g "daemon off;"
