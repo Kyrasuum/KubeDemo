5c6920ef34557f5d9324b1a7f91e8b029bac5964
Phillip Lorentz <plorentz7@gmail.com>
2025-01-28 10:53:30 -0600

    Update App.vue
    adding some template code

--- web/internal/pages/src/App.vue
+++ web/internal/pages/src/App.vue
@@ -1,6 +1,28 @@
 <script setup lang="ts">
 import { useQuasar } from "quasar";
 
+export const call = (f:any) => typeof f === 'function'? f(): f;
+
+export const parallel = (...fns:any[])  => () => Promise.all(fns.map(call));
+
+export const series = (...fns:any[]) => async () => {
+	let res = [];
+
+	for (let f of fns)
+		res.push(await call(f));
+
+	return res;
+};
+
+export function until(conditionFunction: any) {
+	const poll = (resolve: any) => {
+		if(conditionFunction()) resolve();
+		else setTimeout(_ => poll(resolve), 400);
+	}
+	return new Promise(poll);
+}
+
+	
 const q = useQuasar();
 let init = 0;
 if (init < 1) {
